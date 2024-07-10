7967d38665b32e6c624007457d9f7d7f8e318451
HalalGuys1232 <ilikechickenkebab@gmail.com>
2024-07-10 17:14:24 +0900

    feat: 구글 로그인 기능 추가

--- apps/userinfo/models.py
+++ apps/userinfo/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     profile = models.ImageField(upload_to='profiles/', default='profiles/default.png', blank=False)
-    nickname = models.CharField(max_length=30, unique=True)
-    email = models.EmailField(unique=True)
-    telnum = models.CharField(max_length=15, unique=False, null=True, blank=True)
+    nickname = models.CharField(max_length=30)
+    email = models.EmailField(null=True, blank=True)
+    telnum = models.CharField(max_length=15, null=True, blank=True)
 
     def __str__(self):
         return self.username
--- config/urls.py
+++ config/urls.py
@@ -18,10 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("apps.travel.urls")),
-    path('mypage/', include("apps.userinfo.urls")),
-#    path('accounts/', include('allauth.urls')),
+    path('mypage/', login_required(TemplateView.as_view(template_name='mypage.html')), name='mypage'),
+    path('accounts/', include('allauth.urls')),
+    path('accounts/profile/', login_required(TemplateView.as_view(template_name='profile.html')), name='profile'),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
